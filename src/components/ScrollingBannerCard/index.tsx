6ab18c7937514e2d73ee8bc6cb69386efff5969d
Max Scholz <max.scholz@maximize-it.eu>
2021-07-10 14:36:42 +0200

    CSS updated

--- src/components/ScrollingBannerCard/index.tsx
+++ src/components/ScrollingBannerCard/index.tsx
@@ -1,6 +1,5 @@
 import React, { FunctionComponent } from "react";
 import styled, { keyframes } from "styled-components";
-import CSS from "csstype";
 
 /**
  * React Component
@@ -17,26 +16,26 @@ export interface ScrollingBannerCardProperties {
 
 export const ScrollingBannerCard: FunctionComponent<ScrollingBannerCardProperties> = (props) => {
 
-  const bannerStyle: CSS.Properties = {
-    width: "100%",
-    overflow: "hidden",
-    whiteSpace: "nowrap",
-  };
+  const Banner = styled.div`
+    width: 100%;
+    overflow: hidden;
+    white-space: nowrap;
+  `
 
   const marqueeAnimation = keyframes`
     0%   { transform: translate(0, 0); }
     100% { transform: translate(-100%, 0); }
   `
 
-  const contentStyle: CSS.Properties = {
-    display: "inline-block",
-    margin: "0",
-    height: "50px",
-    lineHeight: "50px",
-    paddingLeft: "100%",
-  };
+  const AnimationWrapper = styled.div`
+    display: inline-block;
+    margin: 0;
+    height: 50px;
+    line-height: 50px;
+    paddin-left: 100%;
+  `
 
-  const AnimationDiv = styled.div`
+  const AnimationDiv = styled(AnimationWrapper)`
     animation: ${marqueeAnimation} ${props.animationspeed}s linear infinite;
     &:hover {
       animation-play-state: paused;
@@ -52,8 +51,8 @@ export const ScrollingBannerCard: FunctionComponent<ScrollingBannerCardPropertie
   `
 
   return (
-    <div style={bannerStyle} className="cwScrollingBannerComponentsScollingBannerCard">
-      <AnimationDiv style={contentStyle}  className="cwScrollingBannerComponentsScollingBannerCardAnimation"><strong>{props.announcementtitle}:</strong> {props.announcementmessage} <Link  className="cwScrollingBannerComponentsScollingBannerCardLink" href={props.announcementlink} target="_blank" rel="noopener noreferrer">{props.announcementlinktitle}</Link></AnimationDiv>
-    </div>
+    <Banner className="cwScrollingBannerComponentsScollingBannerCard">
+      <AnimationWrapper className="cwScrollingBannerComponentsScollingBannerCardAnimationWrapper"><AnimationDiv className="cwScrollingBannerComponentsScollingBannerCardAnimation"><strong>{props.announcementtitle}:</strong> {props.announcementmessage} <Link  className="cwScrollingBannerComponentsScollingBannerCardLink" href={props.announcementlink} target="_blank" rel="noopener noreferrer">{props.announcementlinktitle}</Link></AnimationDiv></AnimationWrapper>
+    </Banner>
   );
 };
