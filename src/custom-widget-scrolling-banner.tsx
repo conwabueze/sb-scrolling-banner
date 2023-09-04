21219081e4f3f8bd9022585effa0ceec737cd814
conwabueze <chinonso1nwabueze@gmail.com>
2023-09-04 11:01:33 -0400

    remove of demo message

--- src/custom-widget-scrolling-banner.tsx
+++ src/custom-widget-scrolling-banner.tsx
@@ -44,16 +44,6 @@ export const CustomWidgetScrollingBanner = ({ title, showtitle, titlecolor, bann
           contentLanguage,
         }}
     />
-    <br /><br />
-    <div data-widget-conf-grid-type="100" data-widget-type="Section" data-widget-src="internal://staffbase.content.widgets.Section">
-      <div>
-        <div data-widget-conf-type="warning" data-widget-on-card="true" data-widget-type="Infobox" data-widget-src="internal://staffbase.content.widgets.Infobox">
-          <p>PROTOTYPES AND FOR DEMO PURPOSES ONLY!</p>
-          <p>NOT FOR PRODUCTION USE!</p>
-          <p>NOT AVAILABLE TO CUSTOMERS!</p>
-        </div>
-      </div>
-    </div>
   </div>
 };
 
